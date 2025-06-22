bd5cf940ba1bbe64f1e5bc786c5f8a571f217ec5
Richard Holland <richard@richardholland.com>
2025-06-22 21:04:16 +0100

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Richard Holland on 22/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
